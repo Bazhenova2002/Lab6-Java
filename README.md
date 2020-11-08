1c46e1e1dc446278ac929564587c070bd171375a
vuser <nxnxngh@gmail.com>
2020-11-08 20:09:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab6-Java
+<h3>Description:</h3>
+<a href="https://github.com/DrViruss/Lab5">Lab5</a> with JavaFX.
+Added context menu for search astronaut or craft on <a href="https://google.com">Google</a>
+# Lab6-Java
---
+++ src/com/anna/lab6/Msg.java
@@ -0,0 +1,28 @@
+package com.anna.lab6;
+
+public class Msg {
+        private String message;
+        private String status;
+
+
+        // Getter Methods
+
+        public String getMessage() {
+            return message;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        // Setter Methods
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+}
